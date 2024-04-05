f51c00589b196e5df6396fd5fc27a1499dc324d0
camfeitosa <camilafeitosacoutinho@gmail.com>
2024-04-05 10:17:49 -0300

    aula1

---
+++ js/objetos.js
@@ -0,0 +1,12 @@
+let aluno1 = {
+    ra: 558808,
+    nome: "Camila",
+    curso: "Engenharia de Software",
+    ativo: true
+};
+
+console.log(aluno1.ra)
+
+aluno1.dataNascimento = "05/07/2006"
+
+console.log(aluno1.dataNascimento)
---
+++ js/variavel.js
@@ -0,0 +1,9 @@
+var mensagem = "Hello World"
+
+console.log(mensagem)
+
+const idade = 21
+
+// ? entao | : else
+const maiorDeIdade = idade >= 18 ? console.log("Maior de idade") : console.log("Menor de idade")
+
