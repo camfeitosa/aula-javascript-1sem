0b060cd7f5fbbcdb2f0661e64a909ed4407b2af9
camfeitosa <camilafeitosacoutinho@gmail.com>
2024-04-28 19:38:42 -0300

    estrutura

--- calculadora/interacaoDOM.js
+++ calculadora/interacaoDOM.js
@@ -4,15 +4,18 @@ let n3 = document.getElementById('n3')
 let n4 = document.getElementById('n4')
 let n5 = document.getElementById('n5')
 let n6 = document.getElementById('n6')
+let n7 = document.getElementById('n7')
+let n8 = document.getElementById('n8')
 
 let r = document.getElementById('r')
 let resultadoSub = document.getElementById('resultadoSub')
 let resultadoMult = document.getElementById('resultadoMult')
+let resultadoDiv = document.getElementById('resultadoDiv')
 
 function soma(){
     let resultado = n1.valueAsNumber + n2.valueAsNumber
     if(isNaN(resultado))
-        r.textContent = "Não digitou nada"
+        alert("Não digitou nada")
         
     else
         r.textContent = resultado
@@ -22,7 +25,7 @@ function soma(){
 function sub(){
     let resultado = n3.valueAsNumber - n4.valueAsNumber
     if(isNaN(resultado))
-        resultadoSub.textContent = "Não digitou nada"
+        alert("Não digitou nada")
         
     else
         resultadoSub.textContent = resultado
@@ -32,10 +35,19 @@ function sub(){
 function mult(){
     let resultado = n5.valueAsNumber * n6.valueAsNumber
     if(isNaN(resultado))
-        resultado.textContent = "Não digitou nada"
+        alert("Não digitou nada")
 
     else
         resultadoMult.textContent = resultado
         resultadoMult.style.color = 'red'
+}
 
+function div(){
+    let resultado = n7.valueAsNumber * n8.valueAsNumber
+    if(isNaN(resultado))
+        alert("Não digitou nada")
+
+    else
+        resultadoDiv.textContent = resultado
+        resultadoDiv.style.color = 'red'
 }
